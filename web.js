5a5db8b3b4437382f075f32bc579d6881bb08b50
Ubuntu <ubuntu@ip-172-31-21-198.sa-east-1.compute.internal>
2013-07-10 23:50:05 +0000

    Added Hello World from index

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-fs.readFile('index.html',function(err,data)){
+
+fs.readFile('index.html',function(err,data){
     if (err) return console.log(err.stack);
     app.get('/', function(request, response) {
 	response.send(data.toString());
