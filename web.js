d0bfecd3ed09c2544d218ec566f14e4495c1319f
Sofia <palamarchuk.sofia@gmail.com>
2013-07-24 00:48:06 +0000

    Edited product in index.html

--- web.js
+++ web.js
@@ -1,15 +1,12 @@
-#!/usr/bin/env node
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var htmlfile = "index.html";
 var fs = require('fs');
 
-fs.readFile('index.html',function(err,data){
-    if (err) return console.log(err.stack);
-     app.get('/', function(request, response) {
-	response.send(data.toString());
-    });
+app.get('/', function(request, response) {
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
 var port = process.env.PORT || 8080;
